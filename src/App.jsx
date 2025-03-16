da90c2cc80336a8d85b9698a86d77a743e246f71
Kaveesha Nethmi <nethmikavee40@gmail.com>
2025-03-16 17:38:21 +0530

    add paths for inventory

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import './App.css'
 
 import Login from './Components/HomeIndexLogin/IndexLogin.jsx'
 import UserServices from './Components/TrackServicesHome/UserServices.jsx'
+import AddInventory from './Components/HomeInventory/addInventory.jsx'
+import ViewInventory from './Components/HomeInventory/viewInventory.jsx'
+import UpdateInventory from './Components/HomeInventory/updateInventory.jsx'
+import ViewOneInventory from './Components/HomeInventory/ViewOneInventory.jsx'
 
 import {
     createBrowserRouter,
@@ -17,6 +21,22 @@ const router = createBrowserRouter([
     {
         path: '/user-services',
         element: <div> <UserServices /> </div>
+    },
+    {
+        path: '/add-in',
+        element: <div> <AddInventory /> </div>
+    },
+    {
+        path: '/view-in',
+        element: <div> <ViewInventory /> </div>
+    },
+    {
+        path: '/update-in/:id',
+        element: <div> <UpdateInventory /> </div>
+    },
+    {
+        path: '/view-one-in/:id',
+        element: <div> <ViewOneInventory /> </div>
     },
 ])
 
